85d6cd9c57748255744ae055395f785582b73657
yukiko <117987266+iam-yukiko@users.noreply.github.com>
2023-05-05 22:32:52 +0900

    課題提出

---
+++ study/src/study/Test1.java
@@ -0,0 +1,23 @@
+package study;
+
+public class Test1 {
+	public static void main(String[] args) {
+		
+//	1.数字の5と9の変数を定義し、足した数字を出力しなさい。
+		int n = 5;
+		int i = 9;
+		
+		System.out.println(n + i);
+		
+//		2.64ビット整数の「1111111111」を定義し2倍した値を出力しなさい。
+		long num;
+		num = 1111111111L;
+		
+		System.out.println(num*2);
+		
+//		3.boolean型の変数の値に不正な文字列比較を入れてfalseを出力してください。	
+		boolean b = false;
+		
+		System.out.println(b);
+	}
+}
