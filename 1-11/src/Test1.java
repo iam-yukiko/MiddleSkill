6c5ba9e0097e4ddabe2ca956a6a54f1c3fcf2eef
yukiko <117987266+iam-yukiko@users.noreply.github.com>
2023-05-19 14:46:40 +0900

    課題提出

--- 1-11/src/Test1.java
+++ 1-11/src/Test1.java
@@ -13,25 +13,35 @@ public class Test1 {
 		//問題2:変数"numb"の値が正の場合は"positive"、負の場合は"negative"、0の場合は"zero"と出力するプログラムを三項演算子を用いて作成してください。
 		//(String result変数を用いてそこに値を格納しそれを出力する形式でお願いいたします)
 		int numb = 5;
-		String result = "";
-//改行
-	System.out.println("問３");
-//問題3:"numc","numd"2つの変数の大きい方の値を出力するプログラムを三項演算子を用いて作成してください。
-//(int intResult変数を用いてそこに値を格納しそれを出力する形式でお願いいたします)
-	//※※必ず全パターン正常動作することをコンソールで確認して下さい。
-	
-int numc = 1;
-int numd = 2;
-int intResult = 0;
-//改行
-	System.out.println("問４");
-//問題4:"nume"と"numf"の変数が両方の正の場合は"both positive"、どちらかの値が正の場合は"one positive"、
-//両方の値が負の場合は"both negative"、それ以外は"etc"と出力するプログラムを三項演算子を用いて作成してください。
-//(String results変数を用いてそこに値を格納しそれを出力する形式でお願いいたします)
-//※※必ず全パターン正常動作することをコンソールで確認して下さい。
-int nume = 4;
-int numf = -3;
-String results = "";
-	}
+		String result = "3";
+		result = (numb == 5 ) ? ( numb > 0) ? "positive" : "negative" : "zero";
+        System.out.println(result);
+		
+		//改行
+		System.out.println("問３");
+		//問題3:"numc","numd"2つの変数の大きい方の値を出力するプログラムを三項演算子を用いて作成してください。
+		//(int intResult変数を用いてそこに値を格納しそれを出力する形式でお願いいたします)
+			//※※必ず全パターン正常動作することをコンソールで確認して下さい。
+		int numc = 1;
+		int numd = 2;
+		int intResult = numc < numd ? 2:1;
+		System.out.println(intResult);
+		//改行
+		
+		System.out.println("問４");
+		//問題4:"nume"と"numf"の変数が両方の正の場合は"both positive"、どちらかの値が正の場合は"one positive"、
+		//両方の値が負の場合は"both negative"、それ以外は"etc"と出力するプログラムを三項演算子を用いて作成してください。
+		//(String results変数を用いてそこに値を格納しそれを出力する形式でお願いいたします)
+		//※※必ず全パターン正常動作することをコンソールで確認して下さい。
+		int nume = 4;
+		int numf = -3;
+		String results = "";
+		result = (nume > 0 && numf > 0) ? "both positive" :
+			(nume > 0 || numf > 0) ? "one positive":
+			(nume < 0 && numf < 0) ? "both negative" : "etc" ;
+		
+		System.out.println(result);
+		
+		}
 
 }
