4d9b70b435c2e157c18ee30a055ab797dd0101a4
yukiko <117987266+iam-yukiko@users.noreply.github.com>
2023-05-24 14:49:43 +0900

    1-12課題提出

--- 1-12/src/Test12.java
+++ 1-12/src/Test12.java
@@ -11,22 +11,29 @@ public class Test12 {
 	int y = 80;
 	int z = 15;
 	if(x > 0 && y > 0 && z > 0 ) { 
-	} else {
 		if(x % 10 == 0) {
-		} else {
 			if(x + y + z == 100) {
-				System.out.println("プラス100");
+				System.out.println("プラス100：OK");
+			} else {
+				System.out.println("プラス100：NG");
 			}
+		} else {
 			System.out.println("10の倍数：NG");
 		}
+	} else {
+		System.out.println("正の整数：NG");
 	}
 	
-		
 		//改行
 	System.out.println("問２");
 		//問題2:問1と同じ内容で三項演算子で条件式を作成しなさい。最後に変数aを出力する。
 	String a = "";
-
+	a = x > 0 && y > 0 && z > 0  ? (x % 10 == 0 ? (x + y + z == 100 ?
+			"プラス100：OK" : "プラス100：NG" ) : "10の倍数：NG" ) : "正の整数：NG" ;
+		 
+	System.out.println(a);
+			
+		
 	}
 }
 
